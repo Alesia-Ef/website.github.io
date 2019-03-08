79009cfbcbeadf3ed9f2cee630a32e5b63079904
Alesia-Ef <41832422+Alesia-Ef@users.noreply.github.com>
2019-03-08 23:26:10 +0200

    Create site

---
+++ website/js/one.js
@@ -0,0 +1,89 @@
+/*var name = prompt('What is your name?', '');
+if (name != 'Alesia') {
+	alert('Wrong');
+}
+*/
+
+/*var value = prompt('Your age?', 18);
+
+var message = (value < 18) ? 'What`s a pitty' :
+(value < 21) ? 'Good age' :
+(value < 100) ? 'Hoorah!':
+'wow'
+
+alert(message);*/
+
+/*var company = prompt('What company did JS?', '');
+
+if (company == 'EcmaScript') {
+	alert('Right');
+} else {
+	alert('wrong');
+}*/
+
+/*var num = prompt('what a value?', '');
+if (num > 0) {
+	alert(1);
+} else if (num < 0) {
+	alert(-1);
+}
+	else {
+		alert(0);
+	}*/
+	
+	/*var login = prompt("Who is come?", "");
+	if (login == 'admin') {
+		var pass = prompt('password', '');
+		
+		if (pass == "black") {
+			alert('welcome');
+		} else if (pass == null) {
+			alert('canceled');
+		} else {
+				alert('password wrong');
+			} 
+		} else if (login == null) {
+			alert('enter canceled');
+		} else {
+			alert('I dont know youe');
+		}*/
+		
+	/*	var result = prompt('write result', '');
+	result = (a + b < 4) ? 'mallo' : 'mnogo';	
+		*/
+		
+	/*	var message = (login == 'Вася') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте' :
+  (login == '') ? 'Нет логина' :
+  '';*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
